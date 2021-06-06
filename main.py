84720b6b3a8b980700f483446e90d1ca4f17a8fe
Matt Welsh <mdw@mdw.la>
2021-06-06 13:38:34 -0700

    Adding tests.

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hi I am a Derpy Search Engine"
+
+
+# For local testing only.
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
